e1d98757fdc8938ac7e180c1dd0db28974d0ae90
Yuvalfriedman1973 <kzyuval@gmail.com>
2024-11-14 15:15:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const banner = document.querySelector(".dynamic-banner");
+    const bannerMessages = [
+        "ניוזלטר מחלקת לוגיסטיקה - עדכונים חשובים",
+        "חדשות החודש במחלקת לוגיסטיקה",
+        "כתבות נבחרות לחודש זה",
+        "מידע ופרויקטים במחלקה"
+    ];
+    let index = 0;
 
+    setInterval(() => {
+        banner.textContent = bannerMessages[index];
+        index = (index + 1) % bannerMessages.length;
+    }, 3000);
+});
